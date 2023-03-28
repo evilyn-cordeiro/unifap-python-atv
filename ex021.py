7abd9f51dd9e3b365d2b3baae35c2cadbbdc73d8
evilyn <evilyndeveloper@gmail.com>
2023-03-28 20:41:50 -0300

    exercício 021 feito

---
+++ ex021.py
@@ -0,0 +1,23 @@
+# Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+# ------------------------------------
+# Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
+# Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
+# Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+# ------------------------------------
+# Entrada
+# O arquivo de entrada contém um valor inteiro.
+
+# Saída
+# Imprima a saída conforme exemplo fornecido.
+
+dias = input()
+dias = int(dias)
+
+anos = (dias // 365)
+meses = (dias % 365) // 30
+diasr = (dias % 365) % 30
+
+print('{} ano(s)'.format(anos))
+print('{} mes(es)'.format(meses))
+print('{} dia(s)'.format(diasr))
+
