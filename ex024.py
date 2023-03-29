7f6e1194f04cbad3a7a089fc3596bf48a85a4099
evilyn <evilyndeveloper@gmail.com>
2023-03-28 23:55:46 -0300

    ex024

---
+++ ex024.py
@@ -0,0 +1,9 @@
+# beecrowd | 2413
+# Busca na Internet
+# -------------------------------
+# o número de pessoas que clicam no segundo link listado é o dobro de número de pessoas que clicam no terceiro link listado
+# o número de pessoas que clicam no segundo link é a metade do número de pessoas que clicam no primeiro link.
+
+t = int(input())
+
+print( t * 4)
