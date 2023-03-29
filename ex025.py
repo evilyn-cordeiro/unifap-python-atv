4fd95f31175afcdfe25a66bd0736b7fb4249a0aa
evilyn <evilyndeveloper@gmail.com>
2023-03-29 10:40:50 -0300

    exercício 025

---
+++ ex025.py
@@ -0,0 +1,15 @@
+# beecrowd | 2006
+# Identificando o Chá
+
+T = int(input())
+A, B, C, D, E = input().split()
+
+A = int(A)
+B = int(B)
+C = int(C)
+D = int(D)
+E = int(E)
+
+numeros = [A,B,C,D,E]
+
+print(numeros.count(T))
