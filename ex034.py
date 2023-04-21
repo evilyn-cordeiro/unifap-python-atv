e640a9cecb05baecba702227004910842dfb87c0
evilyn <evilyndeveloper@gmail.com>
2023-04-21 14:39:32 -0300

    primeiras de abril

---
+++ ex034.py
@@ -0,0 +1,11 @@
+# beecrowd | 1011
+# Esfera
+# Adaptado por Neilor Tonin, URI  Brasil
+
+# Timelimit: 1
+
+raio = float(input())
+pi = 3.14159
+volume = (4/3.0) * pi * raio**3
+
+print('VOLUME = %.3f' %volume)
---
+++ ex035.py
@@ -0,0 +1,11 @@
+# beecrowd | 1014
+# Consumo
+# Adaptado por Neilor Tonin, URI  Brasil
+
+# Timelimit: 1
+distancia_total = int(input())
+gasto_combustivel = float(input())
+
+consumo_medio = distancia_total / gasto_combustivel
+
+print('%.3f km/l' %consumo_medio)
